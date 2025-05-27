8984d5265c0a7449855ac9c6aead56b44f31be0d
rafanduba <rafaelrocha241003@gmail.com>
2025-05-26 22:07:13 -0300

    Tudo corrigido

--- simulation/qsim/work/@u@l@a_1bit_vlg_check_tst/_primary.vhd
+++ simulation/qsim/work/@u@l@a_1bit_vlg_check_tst/_primary.vhd
@@ -2,8 +2,8 @@ library verilog;
 use verilog.vl_types.all;
 entity ULA_1bit_vlg_check_tst is
     port(
-        carry           : in     vl_logic;
-        \out\           : in     vl_logic;
+        COUT            : in     vl_logic;
+        S               : in     vl_logic;
         sampler_rx      : in     vl_logic
     );
 end ULA_1bit_vlg_check_tst;
---
+++ simulation/qsim/work/ula_1bit_vlg_vec_tst/_primary.vhd
@@ -0,0 +1,4 @@
+library verilog;
+use verilog.vl_types.all;
+entity ula_1bit_vlg_vec_tst is
+end ula_1bit_vlg_vec_tst;
