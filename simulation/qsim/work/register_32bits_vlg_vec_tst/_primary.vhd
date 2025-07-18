e2164da439f39b54878d9450c686e0b68493a672
Rafael <rafaelrocha241003@gmail.com>
2025-07-18 13:01:58 -0300

    Correção

--- simulation/qsim/work/register_32bits_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity register_32bits_vlg_vec_tst is
-end register_32bits_vlg_vec_tst;
--- simulation/qsim/work/shifter_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity shifter_vlg_vec_tst is
-end shifter_vlg_vec_tst;
